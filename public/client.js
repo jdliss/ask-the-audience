52a06091528c36e5c0ef88c0f3c863a4af1a8cd4
Jonathan <jonacom@lissismore.com>
2016-07-19 23:05:03 -0600

    commit

---
+++ public/client.js
@@ -0,0 +1,27 @@
+var socket = io();
+
+var connectionCount = document.getElementById('connection-count');
+
+socket.on('usersConnected', function (count) {
+  connectionCount.innerText = 'Connected Users: ' + count;
+});
+
+var statusMessage = document.getElementById('status-message');
+
+socket.on('statusMessage', function (message) {
+  statusMessage.innerText = message;
+});
+
+var buttons = document.querySelectorAll('#choices button');
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function () {
+    socket.send('voteCast', this.innerText);
+  });
+}
+
+socket.on('voteCount', function (votes) {
+  var text = "A: " + votes.A + ",  " + "B: " + votes.B + ",  " + "C: " + votes.C + ",  " + "D: " + votes.D
+  document.getElementById('vote').innerHTML = "<br>you just voted: " + text;
+  debugger;
+});
